4f87bcf25071c33a7ab66a4f3f00e8d32b130b9e
Juel B <juel.braganza97@gmail.com>
2021-03-24 22:48:56 +0000

    select elements + add event listeners

---
+++ script.js
@@ -0,0 +1,15 @@
+// select the elements
+const initialVal = document.querySelector('#input1')
+const finalVal = document.querySelector('#input2')
+const calcButton = document.querySelector('#btn')
+
+// event listeners
+initialVal.addEventListener('keyup', function () {
+
+})
+finalVal.addEventListener('keyup', function () {
+
+})
+calcButton.addEventListener('click', function () {
+
+})
